393642094a66e0d4e5b0001f15c4253dba171b54
schm00g <hk8igureu@relay.firefox.com>
2022-05-17 19:09:10 +0100

    layout tweaks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,10 @@ import { getOptionsForVote } from "@/utils/getRandomPokemonHelper";
 import { trpc } from "@/utils/trpc";
 import { useEffect, useState } from "react";
 import type React from "react";
+import { inferQueryResponse } from "./api/trpc/[trpc]";
 
 const btn =
-  "inline-flex items-center px-3 mt-5 py-1.5 border border-gray-300 shadow-sm font-medium rounded-full text-gray-700 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500";
+  "inline-flex items-center px-3 ml-5 mt-5 py-1.5 border border-gray-300 shadow-sm font-medium rounded-full text-gray-700 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500";
 
 export default function Home(){
     
@@ -32,17 +33,17 @@ export default function Home(){
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center relative">
       <div className="text-2xl text-center">Which Pokémon is more round?</div>
-      <div className="p-2"/>
       <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
         { !firstPokemon.isLoading && 
           !secondPokemon.isLoading &&
           firstPokemon.data &&
           secondPokemon.data && (
-            <>
+            <div className="p-8 flex justify-between items-center max-w-2xl flex-col md:flex-row animate-fade-in">
               <PokemonListing pokemon={firstPokemon.data} vote={() => voteForRoundest()}/>
               <div className="p-8">Vs</div>  
               <PokemonListing pokemon={secondPokemon.data} vote={() => voteForRoundest()}/>  
-            </>
+              <div className="p-2"/>
+            </div>
         )}
       </div>
     </div>
@@ -56,7 +57,7 @@ const PokemonListing: React.FC<{pokemon: PokemonFromServer, vote: () => void}> =
     <div>
       <img 
           alt="sprite" 
-          className="w-full" 
+          className="w-32 h-32" 
           src={props.pokemon.sprites.front_default}
       />
       <div className="text-l text-center capitalize pt-1">

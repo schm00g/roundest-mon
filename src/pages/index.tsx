83cce5484258c0f2220ec9d5d2c19538a1c058e0
schm00g <hk8igureu@relay.firefox.com>
2022-05-17 11:59:47 +0100

    setting up tRPC router and query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,17 @@
+import { trpc } from "@/utils/trpc";
 import type React from "react";
 
 export default function Home(){
+  const { data, isLoading } = trpc.useQuery(["hello", { text: "Hello"}]);
+  
+  if(isLoading){
+    return <div>Loading...</div>
+  }
+
+  if(data){
+    return <div>{data.greeting}</div>
+  }
+  
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center relative">
       <div className="text-2xl text-center">Which Pokemon is rounder?</div>

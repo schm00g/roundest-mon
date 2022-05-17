1db8ad1ab41076940274c164f75a628cd603bf33
schm00g <hk8igureu@relay.firefox.com>
2022-05-17 10:32:33 +0100

    layout and styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type React from "react";
 
-const Home: NextPage = () => {
+export default function Home(){
   return (
-    <div className="">Hello World</div>
-  )
+    <div className="h-screen w-screen flex flex-col justify-center items-center relative">
+      <div className="text-2xl text-center">Which Pokemon is rounder?</div>
+      <div className="p-2"/>
+      <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
+        <div className="w-16 h-16 bg-red-200"></div>  
+        <div className="p-8">Vs</div>  
+        <div className="w-16 h-16 bg-pink-200"></div>  
+      </div>
+    </div>
+  );
 }
-
-export default Home

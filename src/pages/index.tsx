d3cd7c13371f88571b747319f457e20c5ea8e510
schm00g <hk8igureu@relay.firefox.com>
2022-05-17 13:43:34 +0100

    logic to determine random 1-d array

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,20 @@
-import { trpc } from "@/utils/trpc";
+import { getOptionsForVote } from "@/utils/getRandomPokemonHelper";
+// import { trpc } from "@/utils/trpc";
+import { useEffect, useState } from "react";
 import type React from "react";
 
 export default function Home(){
-  const { data, isLoading } = trpc.useQuery(["hello", { text: "Hello"}]);
-  
-  if(isLoading){
-    return <div>Loading...</div>
-  }
+    
+  const [first, second] = getOptionsForVote();
 
-  if(data){
-    return <div>{data.greeting}</div>
-  }
-  
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center relative">
       <div className="text-2xl text-center">Which Pokemon is rounder?</div>
       <div className="p-2"/>
       <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-        <div className="w-16 h-16 bg-red-200"></div>  
+        <div className="w-16 h-16 bg-red-500">{first}</div>  
         <div className="p-8">Vs</div>  
-        <div className="w-16 h-16 bg-pink-200"></div>  
+        <div className="w-16 h-16 bg-pink-600">{second}</div>  
       </div>
     </div>
   );

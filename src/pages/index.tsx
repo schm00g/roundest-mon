5765b3a6c676ad914cbdf33787e7935b18d3785a
schm00g <hk8igureu@relay.firefox.com>
2022-05-17 14:05:19 +0100

    fix hydration issue with react hooks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,16 +5,20 @@ import type React from "react";
 
 export default function Home(){
     
-  const [first, second] = getOptionsForVote();
+  const [options, setOptions] = useState<Array<number>>([]);
+
+  useEffect(() => {
+    setOptions(getOptionsForVote());
+  }, [])
 
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center relative">
       <div className="text-2xl text-center">Which Pokemon is rounder?</div>
       <div className="p-2"/>
       <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-        <div className="w-16 h-16 bg-red-500">{first}</div>  
+        <div className="w-16 h-16 bg-red-500">{options[0]}</div>  
         <div className="p-8">Vs</div>  
-        <div className="w-16 h-16 bg-pink-600">{second}</div>  
+        <div className="w-16 h-16 bg-pink-600">{options[1]}</div>  
       </div>
     </div>
   );
